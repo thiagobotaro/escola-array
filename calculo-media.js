54818c6247e83fd087c490996b595c14a74d8227
thiagopando <thiagopando06@gmail.com>
2024-03-18 10:46:55 -0300

    Atualização

---
+++ calculo-media.js
@@ -0,0 +1,13 @@
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+
+// índice      0    1   2   3
+const notas = [10, 6.5, 8, 7.5];
+
+// Elementos     10         6.5        8          7.5   -   metodo que forncece o tamanho do array
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(notas.length);
+console.log(media);
---
+++ index.js
@@ -0,0 +1,9 @@
+const clinica = [];
+
+clinica.push('Gato', 'Cachorro', 'Papagaio');
+
+console.log(clinica);
+
+clinica.shift();
+
+console.log(clinica);
