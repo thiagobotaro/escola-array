f05aa69ea96cd3bd7192eb04c22d3554f825075a
thiagopando <thiagopando06@gmail.com>
2024-03-18 11:43:36 -0300

    Update

---
+++ array-de-arrays.js
@@ -0,0 +1,9 @@
+const alunos = ['João', 'Thiago', 'Matheus', 'Simão'];
+const medias = [10, 8, 7.5, 9];
+
+const lista = [alunos, medias];
+
+console.log(
+`O aluno da posição 1 da lista é: ${lista[0][1]}
+A média desse aluno é: ${lista[1][1]}`
+);
