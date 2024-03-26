a20eeca4d5029069a878d7233eac7e1b471140dc
thiagopando <thiagopando06@gmail.com>
2024-03-26 07:18:11 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-numerosPares = [];
+const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14];
+let media = 0;
+let soma = 0;
 
-for (let i = 0; i <= 1000; i += 2){
-    numerosPares.push(i);
+for(let i = 0; i < numeros.length; i++){
+    let elemento = numeros[i];
+    soma += elemento;
 }
 
-console.log(numerosPares);
+media = soma/numeros.length;
+console.log(media);
