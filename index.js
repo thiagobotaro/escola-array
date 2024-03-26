92f565d0c25fab559a2b03735ff7cabd88e57371
thiagopando <thiagopando06@gmail.com>
2024-03-26 07:19:17 -0300

    Merge

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const numeros = [3, 8, 12, 5, 6, 10, 7, 2, 9, 14];
-let media = 0;
-let soma = 0;
-
-for(let i = 0; i < numeros.length; i++){
-    let elemento = numeros[i];
-    soma += elemento;
-}
-
-media = soma/numeros.length;
-console.log(media);
-=======
 function executaOperacaoEmArray(array, callback){
 
 }
->>>>>>> b6a8b04e7a4143566b18ab880817975fda2df09e
