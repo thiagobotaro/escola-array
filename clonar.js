6d12571d6aa55e230d4b3df78f86f9f24c168a22
thiagopando <thiagopando06@gmail.com>
2024-04-01 15:21:46 -0300

    Update da segundona

---
+++ clonar.js
@@ -0,0 +1,8 @@
+const notas = [7, 7, 8, 9];
+const novaListaNotas = [...notas, 10];
+
+// stread operator
+// novaListaNotas.push(10);
+
+console.log(notas);
+console.log(novaListaNotas);
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-function executaOperacaoEmArray(array, callback){
+const numeros = [43, 50, 65, 12];
 
-}
+const soma = numeros.reduce((acum, atual) => acum + atual, 0);
+
+console.log(soma);
